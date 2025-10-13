4123454cbb7fb9c91cd15bbad152274a00aa5272
Баранников Иван ИП-513 <anime.byrynki2019@gmail.com>
2025-10-13 20:24:30 +0700

    Исправление файла exercise1

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    float array[10];
-    float sum = 0;
+    double array[10];
+    double sum = 0;
     
     for(int i = 0; i < 10; i++) {
-        scanf("%f", &array[i]);
+        scanf("%lf", &array[i]);
         sum += array[i];
     }
-    
-    printf("%.2f\n", sum / 10);
-    return 0;
+    double average = sum / 10;
+    average = round(average * 100) / 100;
+    printf("%.2lf\n", average); 
 }
