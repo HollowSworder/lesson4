12e113c9a7f8cda35c9ba31f37c0aa799f63998e
HollowSworder <anime.byrynki2019@gmail.com>
2025-11-03 12:17:25 +0700

    Update exercise3.c

--- src/exercise3.c
+++ src/exercise3.c
@@ -13,6 +13,6 @@ int main(int argc, char** argv) {
     }
     for(int i = 0; i<10; i++){
         int index = (10 - k +i) %10;
-        printf("%.2f\n", num[index]);        
+        printf("%.3f\n", num[index]);        
     }
 }
