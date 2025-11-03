f9bb8b3db4ea9e52422bd89cccaf10d03240673e
HollowSworder <anime.byrynki2019@gmail.com>
2025-11-03 12:17:43 +0700

    Update exercise5.c

--- src/exercise5.c
+++ src/exercise5.c
@@ -27,7 +27,7 @@ int main() {
 
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            printf("%.2f ", C[i][j]);
+            printf("%.3f ", C[i][j]);
         }
     }
     printf("\n");
